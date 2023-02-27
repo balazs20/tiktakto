1097a3b4d7de2953f837cabaec88b8e1e08bba24
Biusspici <83572745+Biusspici@users.noreply.github.com>
2023-02-27 10:20:05 +0100

    Update tictatcto.sh

--- tictatcto.sh
+++ tictatcto.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 valto=true
-j=
+j=x,o
+x=
+y=
 
 while [valto] 
 do
   if [$j -e 'X']
   then
-    
+    echo "x játékos x koordináta:"
+    read x
+    echo "y játékos y koordináta:"
+    read y
   else
     
   fi
